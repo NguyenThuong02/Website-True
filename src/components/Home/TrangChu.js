ae214b80967416bef8b4430fdc87630a74f93265
NguyenThuong02 <nguyenthanhthuong02@gmail.com>
2023-12-08 22:58:28 +0700

    Hoàn thành responsive Home UI

--- src/components/Home/TrangChu.js
+++ src/components/Home/TrangChu.js
@@ -1,4 +1,4 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import human from '../../img/human.png'
 import search from '../../img/iconSearch.svg'
 import search1 from '../../img/iconbase.svg'
@@ -63,6 +63,19 @@ function Home() {
         {src: p4},
         {src: p5},
         {src: p6},
+    ]
+
+    let logoNetwork = [
+        {src: tiktokIcon, href:'https://www.facebook.com/'},
+        {src: fbIcon, href:'https://www.facebook.com/'},
+        {src: ytbIcon, href:'https://www.facebook.com/'},
+    ]
+
+    let LinkFooter = [
+        {name:'Tài liệu hướng dẫn ', link:'/'},
+        {name:'Chính sách bảo mật', link:'/'},
+        {name:'Chính sách thanh toán và bán hàng', link:'/'},
+        {name:'Điều khoản và điều kiện sử dụng', link:'/'},
     ]
 
     return (
@@ -176,7 +189,7 @@ function Home() {
             </div>
 
             {/* Body 3 */}
-            <div className='flex flex-col items-center lg:block relative w-full lg:h-[781px] bg-be shrink-0 px-[var(--margin-mobile)] pt-[27px] lg:pt-[53px] lg:pl-[10.486%] lg:pr-[9.1667%] pb-[6px] lg:pb-[28px]'>
+            <div className='flex flex-col items-center lg:block lg:relative w-full lg:h-[781px] bg-be shrink-0 px-[var(--margin-mobile)] pt-[27px] lg:pt-[53px] lg:pl-[10.486%] lg:pr-[9.1667%] pb-[6px] lg:pb-[28px]'>
                 <div className='w-full flex flex-col gap-[44px] items-center lg:block lg:w-[50%]'>
                     <h2 className="text-[24px] lg:text-[40px]">
                         <span className="text-primary font-lora font-bold not-italic leading-normal">Hệ sinh thái </span>
@@ -193,7 +206,7 @@ function Home() {
                         }
                     </ul>
                 </div>
-                <Link className='flex items-center absolute top-[40%] lg:top-[68px] right-[var(--margin-mobile)] lg:right-[132px] text-[14px] lg:text-[20px] text-secondary font-pop font-semibold' to="/forme">
+                <Link className='flex mt-[15px] ml-[55%] lg:mt-0 lg:ml-0 items-center lg:absolute lg:top-[68px] lg:right-[9.1667%] text-[14px] lg:text-[20px] text-secondary font-pop font-semibold' to="/forme">
                     Xem thêm 
                     <i className="fa fa-chevron-right ml-[8px] mt-[8px] lg:mt-[11px] text-[14px] lg:text-[16px] w-[20px] lg:w-[24px] h-[20px] lg:h-[24px] font-normal"></i>
                 </Link>
@@ -345,7 +358,7 @@ function Home() {
                 </div>
             </div>
 
-            <div className='pt-[47px] lg:pt-[0] pb-[22px] lg:pb-[30px] bg-[#fff] lg:bg-[#f1f1f1]'>
+            <div className='pt-[47px] border-b lg:border-none lg:pt-[0] pb-[22px] lg:pb-[30px] bg-[#fff] lg:bg-[#f1f1f1]'>
                 {/* Body 5 */}
                 <div className='flex flex-col gap-[22px] lg:gap-[56px] px-[var(--margin-mobile)] lg:px-0 py-[15px] lg:pt-[146px] lg:pb-[100px] bg-[#fff] lg:bg-[#f1f1f1]'>
                     <h2 className=" text-[24px] lg:text-[40px] text-center text-primary font-lora font-semibold lg:font-bold not-italic leading-normal">Đối tác của chúng tôi?</h2>
@@ -359,18 +372,18 @@ function Home() {
                 </div>
     
                 {/* Body 6 */}
-                <div className='body6 px-[var(--margin-desktop)] pt-[130px] pb-[132px] w-full h-[624px] flex justify-between shrink-0'>
-                    <div className='flex flex-col items-start gap-[64px] w-[466px]'>
-                        <h2 className="text-[40px] text-primary font-lora font-bold not-italic leading-normal">Liên hệ</h2>
+                <div className=' flex flex-col lg:flex-row lg:justify-between body6 px-[var(--margin-mobile)] lg:px-[var(--margin-desktop)] py-[22px] lg:pt-[130px] lg:pb-[132px] w-full lg:h-[624px] gap-[22px] lg:gap-0  shrink-0'>
+                    <div className='flex flex-col items-center lg:items-start w-full gap-[24px] lg:gap-[64px] lg:w-[466px]'>
+                        <h2 className="text-[24px] lg:text-[40px] text-[#FA541C] lg:text-primary font-lora font-semibold lg:font-bold not-italic leading-normal">Liên hệ</h2>
                         <div className='flex flex-col items-start w-[90%] gap-[16px]'>
-                            <div className='flex items-center gap-[8px] text-[#fff]'>
-                                <img className='w-[24px] h-[24px]' src={iconEmail}/>
+                            <div className='flex lg:items-center gap-[8px] text-[#fff]'>
+                                <img className='w-[24px] h-[24px]' src={iconEmail} alt=''/>
                                 <span className=' font-pop font-normal leading-7 text-[16px]'>
                                     dvkh.trueconnect@gmail.comcom
                                 </span>
                             </div>
-                            <div className='flex items-center gap-[8px] text-[#fff]'>
-                                <img className='w-[24px] h-[24px]' src={iconAdress}/>
+                            <div className='flex lg:items-center gap-[8px] text-[#fff]'>
+                                <img className='w-[24px] h-[24px]' src={iconAdress} alt=''/>
                                 <span className=' font-pop font-normal leading-7 text-[16px]'>
                                     80, 69b/22/45 Hoang Van Thai, Khuong Trung Thanh Xuan, Ha Noi
                                 </span>
@@ -378,12 +391,12 @@ function Home() {
                         </div>
                     </div>
     
-                    <form className='w-[466px] flex flex-col items-start'>
-                        <input className='w-[466px] p-[14px] font-pop text-[14px] text-[#919EAB] font-normal leading-[26px] outline-none rounded-[8px] bg-bginput mb-[20px]' placeholder='Họ và tên' type='text'/>
-                        <input className='w-[466px] p-[14px] font-pop text-[14px] text-[#919EAB] font-normal leading-[26px] outline-none rounded-[8px] bg-bginput mb-[20px]' placeholder='Email của bạn' type='email'/>
-                        <input className='w-[466px] p-[14px] font-pop text-[14px] text-[#919EAB] font-normal leading-[26px] outline-none rounded-[8px] bg-bginput mb-[20px]' placeholder='Số điện thoại' type='text'/>
-                        <input className='w-[466px] p-[14px] font-pop text-[14px] text-[#919EAB] font-normal leading-[26px] outline-none rounded-[8px] bg-bginput' placeholder='Ghi chú' type='text'/>
-                        <button className='w-[172px] mt-[40px] px-[22px] py-[11px] rounded-[8px] bg-primary font-pop text-[15px] font-semibold leading-[26px] text-[#fff]'>
+                    <form className='w-full lg:w-[466px] flex flex-col items-center lg:items-start'>
+                        <input className=' w-full lg:w-[466px] p-[14px] font-pop text-[14px] text-[#919EAB] font-normal leading-[26px] outline-none rounded-[8px] bg-bginput mb-[22px] lg:mb-[20px]' placeholder='Họ và tên' type='text'/>
+                        <input className=' w-full lg:w-[466px] p-[14px] font-pop text-[14px] text-[#919EAB] font-normal leading-[26px] outline-none rounded-[8px] bg-bginput mb-[22px] lg:mb-[20px]' placeholder='Email của bạn' type='email'/>
+                        <input className=' w-full lg:w-[466px] p-[14px] font-pop text-[14px] text-[#919EAB] font-normal leading-[26px] outline-none rounded-[8px] bg-bginput mb-[22px] lg:mb-[20px]' placeholder='Số điện thoại' type='text'/>
+                        <input className=' w-full lg:w-[466px] p-[14px] font-pop text-[14px] text-[#919EAB] font-normal leading-[26px] outline-none rounded-[8px] bg-bginput' placeholder='Ghi chú' type='text'/>
+                        <button className='w-[172px] mt-[22px] lg:mt-[40px] px-[22px] py-[11px] rounded-[8px] bg-primary font-pop text-[15px] font-semibold leading-[26px] text-[#fff]'>
                             Gửi thông tin
                         </button>
                     </form>
@@ -391,52 +404,70 @@ function Home() {
             </div>
 
             {/* Body footer */}
-            <div className='pl-[69.94px] pr-[158px] pt-[62.43px] pb-[50px] flex flex-col items-start gap-[32px]'>
-                <img className='ml-[-20px] w-[231px] h-[60px] shrink-0' src={logo}/>
-                <div className='w-full flex justify-between gap-[100px]'>
+            <div className='px-[var(--margin-mobile)] py-[30px] lg:pl-[4.857%] lg:pr-[10.9722%] lg:pt-[62.43px] lg:pb-[50px] flex flex-col items-center lg:items-start gap-[32px]'>
+                <img className='lg:ml-[-20px] w-[206px] lg:w-[231px] h-[54px] lg:h-[60px] shrink-0' src={logo} alt=''/>
+                <div className='w-full flex flex-col lg:flex-row lg:justify-between lg:gap-[100px]'>
                     {/* Footer Left */}
-                    <div className='flex flex-1 flex-col items-start gap-[20px]'>
-                        <h3 className=' font-pop text-[20px] font-normal leading-5'>
+                    <div className='flex flex-1 flex-col lg:items-start gap-[20px]'>
+                        <h3 className=' hidden lg:block font-pop text-[20px] font-normal leading-5'>
                             CÔNG TY CỔ PHẦN CÔNG NGHỆ TRUECONNECT 
                         </h3>
-                        <ul className='flex flex-col items-start gap-[15px]'>
+                        <h3 className='lg:hidden text-center font-pop text-[20px] font-normal leading-5'>
+                            CTCP CÔNG NGHỆ TRUECONNECT 
+                        </h3>
+                        
+                        <ul className='flex flex-col items-start gap-[12px] lg:gap-[15px]'>
+                            <li className='lg:hidden'>
+                                <span className=' font-pop font-semibold lg:font-normal leading-[26px] text-[13px] text-primary'>Hotline/Zalo</span>
+                                <span className=' font-pop font-normal leading-[26px] text-[13px]'>:</span>
+                                <span className=' font-pop font-normal leading-[26px] text-[13px]'> 0888 136 622</span>
+                            </li>
                             <div className='flex items-center gap-[8px] text-[#000]'>
-                                <img className='w-[20px] h-[20px] ' src={emailFooter}/>
-                                <span className=' font-pop font-normal leading-7 text-[14px]'>
+                                <img className='w-[20px] h-[20px] ' src={emailFooter} alt=''/>
+                                <span className=' font-pop font-normal leading-7 text-[13px] lg:text-[14px]'>
                                     dvkh.trueconnect@gmail.comcom
                                 </span>
                             </div>
                             <div className='flex items-center gap-[8px] text-[#000]'>
-                                <img className='w-[20px] h-[20px]' src={addressFooter}/>
-                                <span className=' font-pop font-normal leading-[26px] text-[14px]'>
+                                <img className='w-[20px] h-[20px]' src={addressFooter} alt=''/>
+                                <span className=' font-pop font-normal leading-[26px] text-[13px] lg:text-[14px]'>
                                     80, 69b/22/45 Hoang Van Thai, Khuong Trung Thanh Xuan, Ha Noi
                                 </span>
                             </div>
-                            <li>
-                                <span className=' font-pop font-normal leading-[26px] text-[14px]'>Hotline/Zalo</span>
+                            <li className=' hidden lg:block'>
+                                <span className=' font-pop font-normal leading-[26px] text-[14px] text-primary'>Hotline/Zalo</span>
                                 <span className=' font-pop font-normal leading-[26px] text-[14px]'>:</span>
-                                <span className=' font-pop font-normal leading-[26px] text-[14px]'>0888 136 622</span>
+                                <span className=' font-pop font-normal leading-[26px] text-[14px]'> 0888 136 622</span>
                             </li>
-                            <li className=' font-pop font-normal leading-[26px] text-[14px]'>
+                            <div className='flex lg:hidden ml-[50%] mt-[15px] mb-[30px] translate-x-[-50%] gap-[6px] lg:gap-[10px]'>
+                                {
+                                    logoNetwork.map(logo => (
+                                        <a href={logo.href}>
+                                            <img className='w-[34px] h-[34px] shrink-0' src={logo.src} alt=''/>
+                                        </a>
+                                    ))
+                                }
+                            </div>
+                            <li className='font-pop font-normal leading-[26px] text-[13px] lg:text-[14px]'>
                                 Số giấy chứng nhận đăng ký kinh doanh:
                             </li>
-                            <li className=' font-pop font-normal leading-[26px] text-[14px]'>
+                            <li className='font-pop font-normal leading-[26px] text-[13px] lg:text-[14px]'>
                                 Ngày cấp: . Nơi cấp: Sở kế hoạch và đầu tư thành phố Hà Nội
                             </li>
                         </ul>
                     </div>
-    
+                        
                     {/* Footer Right */}
                     <div className='flex-1 flex flex-col justify-between'>
-                        <div className='w-full flex justify-between gap-[80px]'>
+                        <div className='hidden w-full lg:flex justify-between gap-[80px]'>
                             <div className='flex-1 flex flex-col items-start gap-[22px]'>
-                                <h4 className=' font-pop text-[13px] text-text1 font-semibold leading-[24px]'>Tài liệu</h4>
+                                <h4 className='font-pop text-[13px] text-text1 font-semibold leading-[24px]'>Tài liệu</h4>
                                 <ul className='flex flex-col items-start gap-[14px]'>
                                     <Link className=' font-pop text-[13px] text-text1 font-normal leading-[24px]' to='/'>Tài liệu hướng dẫn</Link>
                                 </ul>
                             </div>
                             <div className='flex-1 flex flex-col items-start gap-[22px]'>
-                                <h4 className=' font-pop text-[13px] text-text1 font-semibold leading-[24px]'>TrueConnect</h4>
+                                <h4 className='font-pop text-[13px] text-text1 font-semibold leading-[24px]'>TrueConnect</h4>
                                 <ul className='w-[100%] flex flex-col items-start gap-[14px]'>
                                     <Link className=' font-pop text-[13px] text-text1 font-normal leading-[24px]' to='/'>Chính sách bảo mật</Link>
                                     <Link className=' font-pop text-[13px] text-text1 font-normal leading-[24px]' to='/'>Chính sách thanh toán và bán hàng</Link>
@@ -444,20 +475,30 @@ function Home() {
                                 </ul>
                             </div>
                         </div>
-                        <div className=' w-full flex justify-between gap-[10px]'>
-                            <a href='https://www.facebook.com/'>
-                                <img className='w-[50px] h-[50px] shrink-0' src={tiktokIcon}/>
-                            </a>
-                            <a href='https://www.facebook.com/'>
-                                <img className='w-[50px] h-[50px] shrink-0' src={fbIcon}/>
-                            </a>
-                            <a href='https://www.facebook.com/'>
-                                <img className='w-[50px] h-[50px] shrink-0' src={ytbIcon}/>
-                            </a>
-                            <input className='w-[310px] py-[10px] px-[12px] font-pop text-[14px] font-normal rounded-[8px] bg-bginput' placeholder='Email'/>
-                            <button className='flex justify-center items-center py-[4px] px-[10px] w-[48px] h-[48px] bg-primary rounded-[8px]'>
-                                <img className='w-[18px] h-[18px] shrink-0' src={leftIcon}/>
-                            </button>
+                        <div className=' w-full flex flex-col items-center lg:flex-row justify-between gap-[10px]'>
+                            <div className='hidden lg:flex gap-[6px] lg:gap-[10px]'>
+                                {
+                                    logoNetwork.map(logo => (
+                                        <a href={logo.href}>
+                                            <img className='w-[50px] h-[50px] shrink-0' src={logo.src} alt=''/>
+                                        </a>
+                                    ))
+                                }
+                            </div>
+                            <div className='w-full mt-[10px] lg:mt-0 lg:bg-transparent lg:flex-1 flex items-center text-[14px] bg-[#e9e9ec] gap-[10px] rounded-[8px]'>
+                                <input className='w-full outline-none lg:w-[310px] h-[48px] py-[10px] px-[12px] font-pop text-[14px] font-normal rounded-[8px] bg-[#e9e9ec]' placeholder='Email'/>
+                                <button className='flex justify-center items-center mr-[4px] lg:mr-0  py-[4px] px-[10px] w-[37.784px] h-[40px] lg:w-[48px] lg:h-[48px] bg-primary rounded-[8px]'>
+                                    <img className='w-[18px] h-[18px] shrink-0' src={leftIcon} alt=''/>
+                                </button>
+                            </div>
+                            <p className='block lg:hidden font-pop text-[13px] font-normal leading-[24px]'>© 2023 All Right Reserved True Connect</p>
+                        </div>
+                        <div className='lg:hidden flex flex-wrap justify-center gap-x-[12px] gap-y-0'>
+                            {
+                                LinkFooter.map(link => (
+                                    <Link className='font-pop text-[10px] text-[#B4B4B4] font-normal leading-[24px]' to={link.link}>{link.name}</Link>
+                                ))
+                            }
                         </div>
                     </div>
     
